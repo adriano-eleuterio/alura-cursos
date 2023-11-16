81af55a9199a3b14509444cefe3e196867f248ed
adriano-eleuterio <adriano.j.eleuterio@gmail.com>
2023-11-16 11:14:43 -0300

    Utilizando o ListView

--- lib/main.dart
+++ lib/main.dart
@@ -89,19 +89,23 @@ class Editor extends StatelessWidget {
 }
 
 class ListaTransferencias extends StatelessWidget {
+
+  final List<Transferencia> _transferencias = [];
+
   @override
   Widget build(BuildContext context) {
+    _transferencias.add(Transferencia(100.0, 1234));
     return Scaffold(
       appBar: AppBar(
         title: const Text('Transferências'),
         backgroundColor: Colors.purple,
       ),
-      body: Column(
-        children: <Widget>[
-          ItemTransferencia(Transferencia(100.00, 123)),
-          ItemTransferencia(Transferencia(200.00, 234)),
-          ItemTransferencia(Transferencia(300.00, 345)),
-        ],
+      body: ListView.builder(
+        itemCount: _transferencias.length,
+        itemBuilder: (context, indice) {
+          final transferencia = _transferencias[indice];
+          return ItemTransferencia(transferencia);
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -112,6 +116,7 @@ class ListaTransferencias extends StatelessWidget {
           future.then((transferenciaRecebida) {
             debugPrint('chegou no then do future');
             debugPrint('$transferenciaRecebida');
+            _transferencias.add(transferenciaRecebida);
           });
         },
         child: const Icon(Icons.add),
